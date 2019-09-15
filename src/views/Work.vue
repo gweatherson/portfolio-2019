6a7e5039911480bf119532df56b18b82c9b54f6d
glenn <gweatherson@gmail.com>
2019-09-15 08:24:55 -0700

    Add work description and remove alert

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="work-container">
-    <div class="work-container__warning" v-show='toggle'>
-      <div class="work-container__warning-title">Holy hell! We have a warning:</div>
-      <div class="work-container__warning-description">Working with a team at an agency limits portfolio work since it would be unfair to take full credit for projects completed. I am currently working on a few projects where Dribbble designs that catch my eye are turning into code.</div>
-      <button class="work-container__warning-button" @click='toggle = !toggle'>Whatever Pal.</button>
-    </div>
-
     <div class="work-list">
       <div class="work-list__item">
-        <h2 class="work-list__item-title">Project: Dribbble Inspiration</h2>
+        <h2 class="work-list__item-title"><a href="https://glennweatherson.com/projects/dribbble-inspiration/" target="_blank">Project: Dribbble Inspiration</a></h2>
         <img class="work-list__item-image" src="../assets/images/portfolio/01.png" alt="Dribbble Inspiration screen of the home page" />
+        <div class="work-list__item-description">
+          This project is based off a Dribbble design. The work isn't real, there is nothing to buy and no one to contact. This is a opportunity for me to practice habits and build designs outside of team based agency work where full credit cannot be taken.
+        </div>
         <div class="work-list__item-links">
           <a href="https://glennweatherson.com/projects/dribbble-inspiration/" target="_blank" class="work-list__item-link">View Project</a>
           <a href="https://github.com/gweatherson/dribbble-semantics" target="_blank" class="work-list__item-link">View on Github</a>
@@ -22,12 +19,12 @@
 <script>
 
 export default {
-  name: 'work',
   data () {
     return {
       toggle: true,
     }
   },
+  name: 'work',
   beforeCreate: function() {
     document.body.className = 'work';
   },
@@ -38,63 +35,23 @@ export default {
 
 <style lang="scss" scoped>
 .work-container {
-  &__warning {
-    position: absolute;
-    max-width: 50rem;
-    padding: 2rem;
-    border: 4px solid $blue;
-    border-top: 14px solid $blue;
-    background-color: $black;
-    margin-top: 2.8rem;
-    margin-left: 0;
-
-    @media (min-width: 768px) {
-      padding: 4rem 4rem 3rem 4rem;
-      margin-top: 2rem;
-      margin-left: 2rem;
-    }
-  }
-
-  &__warning-title {
-    font-family: 'Space Mono', monospace;
-    text-transform: uppercase;
-    color: $blue;
-    margin-bottom: 1rem;
-  }
-
-  &__warning-description {
-
-  }
-
-  &__warning-button {
-    text-transform: uppercase;
-    padding: 1rem 0;
-    display: block;
-    margin-top: 2rem;
-  }
-
-  button {
-    border: none;
-    background-color: transparent;
-    padding: 0;
-    color: $blue;
-    padding: 1rem 0;
-  }
 }
 
 .work-list {
   max-width: 114rem;
-  width: 100%;
   margin: 0 auto;
-  padding-top: 2rem;
+  margin-top: 2rem;
+
 
   @media (min-width: 768px) {
-    padding-top: 14rem;
+    margin-top: 14rem;
+    width: calc(100% - 4rem);
+    padding-left: 2rem;
+    padding-right: 2rem;
   }
 
   &__item {
     width: 100%;
-    text-align: right;
     img {
       max-width: 100%;
     }
@@ -105,6 +62,27 @@ export default {
     color: $blue;
     font-size: 2rem;
     margin-bottom: 1rem;
+
+    a {
+      color: $blue;
+    }
+  }
+
+  &__item-image {
+    margin-bottom: 2rem;
+
+    @media (min-width: 768px) {
+      margin-bottom: 4rem;
+    }
+  }
+
+  &__item-description {
+    max-width: 60rem;
+    margin-bottom: 2rem;
+
+    @media (min-width: 768px) {
+      margin-bottom: 4rem;
+    }
   }
 
   &__item-links {
@@ -117,9 +95,16 @@ export default {
 
   &__item-link {
     padding: 2rem;
+    color: $blue;
+
+    &:visited {
+      color: $blue;
+    }
 
     &:last-of-type {
-      padding-right: 0;
+      @media (min-width: 768px) {
+        padding-right: 0;
+      }
     }
   }
 }
